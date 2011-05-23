771bc27bf20177238d6c1c85fcee35b7822c2033
Alain Rodriguez <arodriguez@agrosica.com>
2011-05-23 10:59:51 -0400

    Moved the currMasterKey and prevMasterKey objects to global.registration.* to make them accessible from the utils library. Also moved the setMasterKey method definition to utils and adjusted the using modules to simply reference the library.

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ var sha2 = require('../common/SHA2');
 // is a simple helper function to create a new object based on input objects.
 // It has been copied from the *Registration* server source, and so is a
 // candidate for moving into a common library.
-exports.mergeObjs = function () {
+var mergeObjs = function () {
 	var outObj = {};
 	for(var i in arguments) {
 		if(arguments[i] instanceof Object) {
@@ -17,18 +17,31 @@ exports.mergeObjs = function () {
 		}
 	}
 	return outObj;
-}
+};
+exports.mergeObjs = mergeObjs;
+
+// ### The *setMasterKey* method
+// is provided by the *Registration* server a new *masterKey*, and this method
+// simply marks the current key as the old key and the newly-provided key as
+// the master key, and informs the *Registration* server that it was successful
+var setMasterKey = function(masterKey) {
+	global.registration.prevMasterKey = global.registration.currMasterKey;
+	global.registration.currMasterKey = masterKey;
+	return true;
+};
+exports.setMasterKey = setMasterKey;
 
 // ## The *verifyUser* function
 // validates whether or not the user has successfully logged into the
 // *Registration* server, indicating whether or not access should be granted
 // by whatever function calls it.
+// IMPORTANT: This function expects global.registration.currMasterKey and global.registration.prevMasterkey to be defined.
 function verifyUser(user_id, session_code) {
-	var currSessionCode = sha2(currMasterKey + user_id);
+	var currSessionCode = sha2(global.registration.currMasterKey + user_id);
 	if(currSessionCode == session_code) {
 		return true;
 	}
-	currSessionCode = sha2(prevMasterKey + user_id);
+	currSessionCode = sha2(global.registration.prevMasterKey + user_id);
 	if(currSessionCode == session_code) {
 		return true;
 	} else {
